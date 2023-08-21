4043fe2fb02a06a383ac7bf029f939b7c190aefb
Billy Oliver <billy.oliver@just-eat.com>
2023-08-21 14:29:04 +0100

    Changed the workflow to use yarn commands instead

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
-const defaultDisplay = <div>
-    <h1>Hi there!</h1>
-    <hr />
-    <MarketingApp />
-</div>;
-
 export default () => {
-    return  defaultDisplay;
+    return  <div>
+                <h1>Hi there!</h1>
+                <hr />
+                <MarketingApp />
+            </div>;
 };
