f4bf3556b1474969fe8ecb569aa47fa3009abe0e
Billy Oliver <billy.oliver@just-eat.com>
2023-08-21 14:23:22 +0100

    Added change to container to get the workflow to trigger

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
+const defaultDisplay = <div>
+    <h1>Hi there!</h1>
+    <hr />
+    <MarketingApp />
+</div>;
+
 export default () => {
-    return  <div>
-                <h1>Hi there!</h1>
-                <hr />
-                <MarketingApp />
-            </div>;
+    return  defaultDisplay;
 };
