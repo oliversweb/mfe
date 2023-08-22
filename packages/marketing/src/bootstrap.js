73ebdf0b0383e5aaf94faaa8dfcebe5dc745c85d
Billy Oliver <billy.oliver@just-eat.com>
2023-08-22 16:35:08 +0100

    Added marketing.yml deployment

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -14,7 +14,6 @@ const mount = (el) => {
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
-
   if (devRoot) {
     mount(devRoot);
   }
