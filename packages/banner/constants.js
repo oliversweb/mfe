d53468f9a9932edf934e979df79666925c6303c0
Billy Oliver <billy.oliver@just-eat.com>
2023-08-30 16:06:53 +0100

    Added constants.js to hold url

---
+++ packages/banner/constants.js
@@ -0,0 +1,4 @@
+export const CONTENT_URL = 'https://mfe-component.s3.eu-west-1.amazonaws.com/uk-footer.json';
+export const CONTENT_FILEPATH = {
+    uk: process.env.NODE_ENV == 'development' ? '../data/uk-footer.json' : './data/uk-footer.json'
+};
