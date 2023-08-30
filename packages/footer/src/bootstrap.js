5a9067344980585d6e10241a7847867f0176e40a
Billy Oliver <billy.oliver@just-eat.com>
2023-08-30 12:54:52 +0100

    Extended webpack to include local content file

--- packages/footer/src/bootstrap.js
+++ packages/footer/src/bootstrap.js
@@ -8,14 +8,14 @@ const getData = async () => {
     let content = null; 
     try {
       console.log(`Enviroment [${process.env.NODE_ENV}] Content filepath [${CONTENT_FILEPATH['uk']}] Content url [${CONTENT_URL}]`);
-      if (process.env.NODE_ENV !== 'development') {        
+      // if (process.env.NODE_ENV !== 'development') {        
         content = file
         console.log(`Loaded local footer content`, content);
-      } else {
-        let res = await fetch(CONTENT_URL);        
-        content = await res.json();
-        console.log(`Loaded remote footer content`, content);
-      }
+      // } else {
+      //   let res = await fetch(CONTENT_URL);        
+      //   content = await res.json();
+      //   console.log(`Loaded remote footer content`, content);
+      // }
     } catch (error) {
       throw new Error(`Unable to fetch footer content ${error.message}`, error);
     }
