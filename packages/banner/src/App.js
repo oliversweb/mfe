6cf46d73871a77bb715c36f25869546639e2f5f2
Billy Oliver <billy.oliver@just-eat.com>
2023-08-30 16:17:33 +0100

    Added footer back in

--- packages/banner/src/App.js
+++ packages/banner/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 export default ({ data }) => {
+  console.log('Banner data: ', data);
+
   return (
-    <>______________ Banner ______________</>
+    <React.Fragment>
+      <>______________ Banner ______________</>
+      <ul>
+      {data.sections.map((item) => (
+        <li key={item.id}>banner name: {item.name}</li>
+      ))}
+      </ul>
+      <>____________________________________</>
+    </React.Fragment>
   );
 };
