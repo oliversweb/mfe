863e3a91a1b820a600157b38cdcfd100b8f38a1a
Billy Oliver <billy.oliver@just-eat.com>
2023-08-30 15:45:30 +0100

    Removed redundancy

--- packages/banner/src/App.js
+++ packages/banner/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default ({ history }) => {
+export default () => {
   return (
     <>______________ Banner ______________</>
   );
--- packages/banner/src/bootstrap.js
+++ packages/banner/src/bootstrap.js
@@ -1,31 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
-  // If we are running in isolation/localhost we will use the browser history provided otherwise we will create a new memory history with an initial path
-  const history = defaultHistory || createMemoryHistory( {
-    initialEntries: [initialPath],
-  });
-
-  if (onNavigate) {
-    history.listen(onNavigate); 
-  }
-
+const mount = (el) => {
   ReactDOM.render(<App />, el);
-
-  return {
-    onParentNavigate({ pathname: nextPathname}) {      
-      // To stop infinite loop rename the pathname variable and compare it to the nextPathname variable
-      const { pathname } = history.location;
-      if ( pathname !== nextPathname ) {
-        // Only if the pathname is different than the nextPathname we will navigate
-        history.push(nextPathname);
-      }
-    }
-  };
 };
 
 // If we are in development and in isolation,
@@ -34,7 +13,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_banner-dev-root');
 
   if (devRoot) {
-    mount(devRoot, { defaultHistory: createBrowserHistory() });
+    mount(devRoot);
   }
 }
 
